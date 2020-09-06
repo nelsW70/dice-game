1008fd3c6a2d88870cc1474c383f730723648328
Nelson Foltz <nelsfoltz@gmail.com>
2020-09-06 08:16:58 -0400

    add: RollDice component created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Die from './Die';
+import RollDice from './RollDice';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Die face="five" />
-      <Die face="six" />
-      <Die face="three" />
-      <Die face="two" />
+      <RollDice />
     </div>
   );
 }
